54d79356fb928db009f7ed9e1c1f54dc97137c8a
Hassan-1991 <67057211+Hassan-1991@users.noreply.github.com>
2023-03-06 13:44:37 -0600

    Update transcription_readthrough.sh

--- transcription_readthrough.sh
+++ transcription_readthrough.sh
@@ -53,26 +53,50 @@ rm interim*
 
 cat ARNold_REL606.gtf RhoTerm_REL606.gtf | sort -t '        ' -nk 4 > REL606_transterm_motifs.gtf
 
-#Step-2 (post-gmapping):
+#GETFLANKS
 
-#Identify bad maps, multiple maps, and deletions for each evolved genome. Convert remainder to gtf
+bedtools flank -b 100 -s -i REL606_transterm_motifs.gtf -g REL606_genomefile.txt | awk '{ printf "%d.%d\t%s\n",(!a[$10]++? ++c:c),a[$10],$0 }' | cut -f 2- -d '.' | cut -f 1 -d ';' | awk -F '\t' '{OFS=FS}{print $2,$3,$4,$5,$6,$7,$8,$9,$10,$1}' | sed "s/\"\t/_/g" | sed "s/$/\";/g" > REL606_transterm_motifs_100bpflanks.gtf
 
-grep "^>" ../REL606_transterm_motifs.faa | tr -d "^>" | cut -f 1 -d ' ' | sed "s/^/=/g" | sed "s/$/.mrna/g" | sort -u > all_transterm_motif_names.txt
+#GET SEQS
 
-grep "mrna1" something*gff3 | awk -F '\t' '($3=="mRNA")' | sed "s/identity=/identity;/g" | sed "s/coverage=/coverage;/g" | awk -F ';' '($7<80||$9<80)' | cut -f 2 -d "=" | cut -f 1 -d '.' | sed "s/^/=/g" | sed "s/$/.mrna/g" > something_bad_maps.txt
-grep "mrna2" something*gff3 | awk -F '\t' '($3=="mRNA")' | cut -f 2 -d "=" | cut -f 1 -d '.' | sed "s/^/=/g" | sed "s/$/.mrna/g" | sort -u > something_multi_maps.txt
-cat something_bad_maps.txt something_multi_maps.txt | sort -u > something_bad_multi_maps
-grep -v -f something_bad_multi_maps something*gff3 | awk -F '\t' '($3=="mRNA")' | cut -f 1 -d ';' | awk -F '\t' '{OFS=FS}{print $1,$2,$3,$4,$5,$6,$7,$8,"gene_id \""$9,"\";transcript_id \""$9,"\";"}' | sed "s/ID=//g" | sed "s/\t\";/\";/g" > something_transterm_100offset.gtf
-awk -F '\t' '($3=="mRNA")' something*gff3 | cut -f 2 -d '=' | cut -f 1 -d '.' | sed "s/^/=/g" | sed "s/$/.mrna/g" | sort -u > all_something_mapped_motifs
-grep -v -f all_something_mapped_motifs all_transterm_motif_names.txt > something_deleted_motifs.txt
+gffread
 
-ls Ara*gff3 | cut -f 1-3 -d "_" > somethings
+#Gmap to evolved, 50Ks first
 
-sed "s/^/sed \"s\/something\//g" somethings | sed "s/$/\/g\" basic_code_gff3processing.sh >> final_code_gff3processing.sh/g" > interim_code_gff3processing.sh 
-bash interim_code_gff3processing.sh
-bash final_code_gff3processing.sh
+#Next step: Get all cases where the 100-bp flanks map well and once in each genome.
 
-#Step-3 (make gtfs out of applied-gd files):
+#1-2:
+
+awk -F '\t' '($3=="mRNA")' Ara-6_50000gen_11389_transtermmotifs_100bpflanks.gff3 | grep "mrna2" | cut -f 1 -d ';' | cut -f 2- -d '=' | sed "s/\.mrna2//g" | sed "s/^/=/g" | sed "s/$/\.mrna/g" > Ara-6_doublemaps
+grep -v -f Ara-6_doublemaps Ara-6_50000gen_11389_transtermmotifs_100bpflanks.gff3 | awk -F '\t' '($3=="mRNA")' > Ara-6_50000gen_11389_transtermmotifs_100bpflanks_singlemaps.gff3
+
+#2-3:
+
+sed "s/=/;/g" Ara-6_50000gen_11389_transtermmotifs_100bpflanks_singlemaps.gff3 | awk -F ';' '($13<80||$15<80)' | cut -f 2 -d ';' | sed "s/\.mrna1//g" | sort -u | sed "s/^/=/g" | sed "s/$/\.mrna/g" > Ara-6_badmaps
+grep -v -f Ara-6_badmaps Ara-6_50000gen_11389_transtermmotifs_100bpflanks_singlemaps.gff3 | awk -F '\t' '($3=="mRNA")' > Ara-6_50000gen_11389_transtermmotifs_100bpflanks_singlemaps_goodmaps.gff3
+
+#3-4:
+
+cut -f 1 -d ';' Ara-6_50000gen_11389_transtermmotifs_100bpflanks_singlemaps_goodmaps.gff3 | cut -f 9 | cut -f 2- -d '=' | cut -f 1 -d '.' | rev | cut -f 2- -d "_" | rev | sort | uniq -c | grep " 2 " | rev | cut -f 1 -d ' ' | rev | awk '{OFS=""}{print $1,"_1 "$1,"_2"}' | sed "s/ /\n/g" | sed "s/^/=/g" | sed "s/$/\.mrna/g" > Ara-6_singlemaps_goodmaps_bothmaps
+grep -f Ara-6_singlemaps_goodmaps_bothmaps Ara-6_50000gen_11389_transtermmotifs_100bpflanks_singlemaps_goodmaps.gff3 > Ara-6_50000gen_11389_transtermmotifs_100bpflanks_singlemaps_goodmaps_bothmaps.gff3
+
+#4-5:
+
+cut -f 1,2 -d "_" Ara-6_singlemaps_goodmaps_bothmaps | sort -u | sed "s/$/_/g" > Ara-6_singlemaps_goodmaps_bothmaps.txt
+sed "s/$/1/g" Ara-6_singlemaps_goodmaps_bothmaps.txt > Ara-6_singlemaps_goodmaps_bothmaps_upstream.txt
+sed "s/$/2/g" Ara-6_singlemaps_goodmaps_bothmaps.txt > Ara-6_singlemaps_goodmaps_bothmaps_downstream.txt
+
+grep -f Ara-6_singlemaps_goodmaps_bothmaps_upstream.txt Ara-6_50000gen_11389_transtermmotifs_100bpflanks_singlemaps_goodmaps_bothmaps.gff3 | cut -f 1 -d ';' | awk -F '\t' '{OFS=FS}{print $1,$2,$3,$5,$6,$7,$8,$9}' | sed "s/_1\.mrna1/_interval_Ara-6;/g" > test1
+grep -f Ara-6_singlemaps_goodmaps_bothmaps_downstream.txt Ara-6_50000gen_11389_transtermmotifs_100bpflanks_singlemaps_goodmaps_bothmaps.gff3 | cut -f 1 -d ';' | cut -f 4 > test2
+paste test1 test2 | awk -F '\t' '{OFS=FS}{print $1,$2,$3,$4+1,$9-1,$5,$6,$7,$8}' > Ara-6_intervals.gtf
+
+#What are intervals of "bad lengths"? The ideal approach would be to impose different ranges for arnold and rhoterm; but for my first volley I'm imposing a general cutoff of 30 to 100.
+
+awk -F '\t' '{OFS=FS}{print $0,$5-$4}' Ara-6_intervals.gtf | awk -F '\t' '($10>30&&$10<100)' | cut -f 1-9 > goodintervals_interim
+
+gffread -E -w goodintervals_interim.faa -g ../../../../all_genomes/Ara-6_50000gen_11389.fasta goodintervals_interim
+
+#Another step (make gtfs out of applied-gd files):
 
 grep -v "#" something*applied.gd | awk -F "applied_start=" '{print $2}' | cut -f 1 > something_col3
 grep -v "#" something*applied.gd | awk -F "applied_end=" '{print $2}' | cut -f 1 > something_col4
@@ -80,16 +104,10 @@ grep -v "#" something*applied.gd | awk -F '\t' '{OFS="_"}{print $2,$1}' > someth
 paste something_col3 something_col4 something_col9 | sed "s/^/REL606\t.\tCDS\t/g" | awk -F '\t' '{OFS=FS}{print $1,$2,$3,$4,$5,".\t+\t.\tgene_id \"",$6,"\";transcript_id \"",$6,"\";"}' | sed "s/\t\";/\";/g" | sed "s/id \"\t/id \"/g" > something_applied.gtf
 rm something_col*
 
-sed "s/^/sed \"s\/something\//g" somethings | sed "s/$/\/g\" basic_code.sh >> final_code.sh/g" > interim_code.sh
-bash interim_code.sh
-bash final_code.sh
+#Big problem: It doesn't seem like ARNOLD can detect termination signals based on small pieces. It needs the whole genome
 
-#Step-4: Intersect between the two gtf files to determine the mutated motifs
+#Let's pause this here. Neither tool can do this.
+#We need to start this transcription-first. Would be good to do it mutation first but...
+#General way of doing it would be to look at the downstream regions of all cases with mutations.
+#But I only care about the 3' ends of genes though.
 
-bedtools intersect -a ../step2_gmapping_to_evolved/something*offset.gtf -b ../step3_appliedgd_to_appliedgtf/something_applied.gtf -wa > something_mutated_motifs.gtf
-
-sed "s/^/sed \"s\/something\//g" somethings | sed "s/$/\/g\" basic_code_intersect.sh >> final_code_intersect.sh/g" > interim_code_intersect.sh
-bash interim_code_intersect.sh
-bash final_code_intersect.sh
-
-###########RETHINK THIS APPROACH###############
