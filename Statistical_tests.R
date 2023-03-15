017088edc13de5924b33f9d713aced7cb891e2f1
Hassan-1991 <67057211+Hassan-1991@users.noreply.github.com>
2023-03-15 17:38:45 -0500

    Create Statistical_tests.R

---
+++ Statistical_tests.R
@@ -0,0 +1,7 @@
+#For the 400bp windows, I want to test these hypotheses:
+
+1 - Amount of annotated:non-annotated stuff goes up/down with time
+  a) Sum(TPM)? - Basically, what fraction of the 10^6 are unannotated, can be represented as relative
+  b) Mean(TPM)? Average TPM of non-annotated, idk if this is relative to annotated
+2 - Number of annotated and/or no-annotated windows goes up/down with time
+
