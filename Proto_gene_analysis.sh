3f0d8a0b3887c72759b653a77c51d15f52f0f7ec
Hassan-1991 <67057211+Hassan-1991@users.noreply.github.com>
2023-06-07 10:32:57 -0500

    Delete Proto_gene_analysis.sh

--- Proto_gene_analysis.sh
+++
@@ -1,5 +0,0 @@
-#Extract evidence for lack of upstream promoters
-
-awk -F '\t' '($7=="+")' linegraph_29_IDs_REL606.gtf | awk -F '\t' '{OFS=FS}{print $1,$2,$3,$4-300,$4,$6,$7,$8,$9}' > linegraph_29_IDs_REL606_upstream300.gtf
-awk -F '\t' '($7=="-")' linegraph_29_IDs_REL606.gtf | awk -F '\t' '{OFS=FS}{print $1,$2,$3,$5,$5+300,$6,$7,$8,$9}' >> linegraph_29_IDs_REL606_upstream300.gtf
-gffread -E -w linegraph_29_IDs_REL606_upstream300.faa -g ../../../all_genomes/REL606.fasta linegraph_29_IDs_REL606_upstream300.gtf
