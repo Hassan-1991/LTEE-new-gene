bd9d311ee68be4e59cea5a1bb39b90802888f400
Hassan-1991 <67057211+Hassan-1991@users.noreply.github.com>
2023-07-28 11:21:39 -0500

    Update Mutation_analysis_updated.sh

--- Mutation_analysis_updated.sh
+++ Mutation_analysis_updated.sh
@@ -18,7 +18,7 @@ sort -t "_" -nk 3 "$i"_ancestor.gtf -o "$i"_ancestor.gtf
 sort -t "_" -nk 3 "$i"_evolved.gtf -o "$i"_evolved.gtf
 done
 
-#manually check to make sure no bad records
+#manually check to make sure no bad records - cat *gtf | sort -nk4
 
 for i in Ara+1 Ara+2 Ara+3 Ara+4 Ara+5 Ara-1 Ara-2 Ara-3 Ara-4 Ara-5 Ara-6; do
 bedtools intersect -s -a "$i"_evolved.gtf -b ../../../converted_gtf_files/"$i"_50000gen*gtf -wo | awk -F '\t' '(($12=="CDS"||$12=="gene"||$12=="repeat_region"||$12~"RNA"||$12~"fCDS")&&($19>10))' | cut -f 2 -d "\"" > "$i"_genic_windows.txt
@@ -111,77 +111,42 @@ for (variable in variable_names) {
   write_csv(deseq.df2, paste0(variable, "_deseq2_results.csv"))
 }
 
-cat Ara*deseq2*csv | awk -F ',' '($1=="rna"&&$8<0.05&&$5>0)' | grep -v "ECB_" | cut -f 3 -d ',' | sort -u > upregs
+cat 100bp/*deseq*csv 200bp/*deseq*csv | awk -F ',' '($1=="rna"&&$8<0.05&&$5>0)' | grep -v "ECB_" | cut -f 3 -d ',' | sort -u > upregs
 
 #BUILD GMAP
 
 for i in Ara+1 Ara+2 Ara+3 Ara+4 Ara+5 Ara-1 Ara-2 Ara-3 Ara-4 Ara-5 Ara-6
-do
-cat 100bp/*evolved*htseqread*gtf 200bp/*evolved*htseqread*gtf | grep -f upregs | grep "$i" > "$i"_upreg.gtf
-gffread -E -w "$i"_upreg.faa -g ../../all_genomes/"$i"*50000*fasta "$i"_upreg.gtf
-/stor/work/Ochman/hassan/tools/gmap-2021-05-27/bin/gmap -D . -d "$i" -f 2 --gff3-fasta-annotation=1 "$i"_upreg.faa > "$i"_upreg.gff3 #map to evolved genomes
+do sed "s/REL606/"$i"/g" /stor/work/Ochman/hassan/LTEE_analysis/LTEE_data/all_genomes/"$i"*50000gen*fasta >> all_genomes_modded.faa
 done
-
-cat *gff3 | grep "mrna2" | cut -f 2- -d '=' | cut -f 1 -d '.' | sort -u > duplicates
-
-
-
-#For each of these cases except MOBs, extract the mutated base(s) and +/-100bp
-
-#For each mutation, find out whether it's plus_down or minus_up.
-#If plus_down, get the +/-100 region the same way we got the +/-500 for gtf
-
-Ara+1_57_DEL_2086923_6104_minus_up_500
-
-for i in Ara+1 Ara+2 Ara+3 Ara+4 Ara+5 Ara-1 Ara-2 Ara-3 Ara-4 Ara-5 Ara-6; do sed "s/^/$i\t/g" "$i"*_50000gen_[0-9][0-9][0-9][0-9][0-9].gd | sed 's/\t/_/1; s/\t/_/1' | sed "s/\t\./_\./g" | awk -F '_' '{OFS=""}{print $1,"_",$3,"_",$2,"\t";print}' | grep "_\."; done > ANC_gdfiles_modded
-for i in Ara+1 Ara+2 Ara+3 Ara+4 Ara+5 Ara-1 Ara-2 Ara-3 Ara-4 Ara-5 Ara-6; do sed "s/^/$i\t/g" "$i"*50000*applied*gd | sed 's/\t/_/1; s/\t/_/1' | sed "s/\t\./_\./g" | awk -F '_' '{OFS=""}{print $1,"_",$3,"_",$2,"\t";print}' | grep "_\."; done > EVO_gdfiles_modded
-grep "plus" upregs | awk -F '_' '{OFS=""}{print $1,"_",$3,"_",$2,"_"}' > upregs_mod_plus_down
-grep "minus" upregs | awk -F '_' '{OFS=""}{print $1,"_",$3,"_",$2,"_"}' > upregs_mod_minus_up
-
-#Plus_down for ancestor, SNP/INS/MOB:
-grep -f upregs_mod_plus_down ANC_gdfiles_modded | egrep "SNP|INS|MOB" | awk -F '\t' '{OFS=""}{print "REL606\t.\tCDS\t",$3-100,"\t",$3+100,"\t.\t+\t0\ttranscript_id \"",$1,"_plus_down_anc\";gene_id \"",$1,"_plus_down_anc\";"}' | sed "s/_\._/_/g" > test.gtf
-#Plus_down for ancestor, AMP/CON/SUB/DEL:
-grep -f upregs_mod_plus_down ANC_gdfiles_modded | egrep "DEL|CON|SUB|AMP" | awk -F '\t' '{OFS=""}{print "REL606\t.\tCDS\t",$3+$4-100,"\t",$3+$4+100,"\t.\t+\t0\ttranscript_id \"",$1,"_plus_down_anc\";gene_id \"",$1,"_plus_down_anc\";"}' | sed "s/_\._/_/g"  >> test.gtf
-#Minus_up for ancestor, all:
-grep -f upregs_mod_minus_up ANC_gdfiles_modded | awk -F '\t' '{OFS=""}{print "REL606\t.\tCDS\t",$3-100,"\t",$3+100,"\t.\t-\t0\ttranscript_id \"",$1,"_minus_up_anc\";gene_id \"",$1,"_minus_up_anc\";"}' | sed "s/_\._/_/g" >>  test.gtf
-
-#For evolved:
-
-grep -f upregs_mod_plus_down EVO_gdfiles_modded | sed "s/IS1\t-1/IS1_-1/g" | sed "s/IS1\t1/IS1_1/g" | sed "s/IS3\t-1/IS3_-1/g" | sed "s/IS3\t1/IS3_1/g" | sed "s/IS4\t-1/IS4_-1/g" | sed "s/IS4\t1/IS4_1/g" | sed "s/IS150\t-1/IS150_-1/g" | sed "s/IS150\t1/IS150_1/g" | sed "s/IS186\t-1/IS186_-1/g" | sed "s/IS186\t1/IS186_1/g" | awk '{ for (i=1; i<=NF; i++) { if (i == 1 || $i ~ /(applied_start=|applied_end=)/) printf "%s ", $i } printf "\n" }' | sed "s/applied_end=//g" | sed "s/applied_start=//g" | sed "s/ /\t/g" | awk -F '\t' '{OFS=""}{print "REL606\t.\tCDS\t",$2-100,"\t",$2+100,"\t.\t+\t0\ttranscript_id \"",$1,"_plus_down_evo\";gene_id \"",$1,"_plus_down_evo\";"}' | sed "s/_\._/_/g" >> test.gtf
-grep -f upregs_mod_minus_up EVO_gdfiles_modded | sed "s/IS1\t-1/IS1_-1/g" | sed "s/IS1\t1/IS1_1/g" | sed "s/IS3\t-1/IS3_-1/g" | sed "s/IS3\t1/IS3_1/g" | sed "s/IS4\t-1/IS4_-1/g" | sed "s/IS4\t1/IS4_1/g" | sed "s/IS150\t-1/IS150_-1/g" | sed "s/IS150\t1/IS150_1/g" | sed "s/IS186\t-1/IS186_-1/g" | sed "s/IS186\t1/IS186_1/g" | awk '{ for (i=1; i<=NF; i++) { if (i == 1 || $i ~ /(applied_start=|applied_end=)/) printf "%s ", $i } printf "\n" }' | sed "s/applied_end=//g" | sed "s/applied_start=//g" | sed "s/ /\t/g" | awk -F '\t' '{OFS=""}{print "REL606\t.\tCDS\t",$3-100,"\t",$3+100,"\t.\t-\t0\ttranscript_id \"",$1,"_minus_up_evo\";gene_id \"",$1,"_minus_up_evo\";"}' | sed "s/_\._/_/g" >> test.gtf
-
-awk -F "\"" '{OFS=FS}{print $2,$1,$2,$3,$4,$5}' test.gtf | sed "s/\"REL606/_REL606/g" | grep "evo" | cut -f 1,7- -d "_" | sed "s/_REL606\t/\t/g" > test_modified.gtf
-awk -F "\"" '{OFS=FS}{print $2,$1,$2,$3,$4,$5}' test.gtf | sed "s/\"REL606/_REL606/g" | grep "evo" | cut -f 1,7- -d "_" | sed "s/_REL606\t/\t/g" >> test_modified.gtf
-
-for i in Ara+1 Ara+2 Ara+3 Ara+4 Ara+5 Ara-1 Ara-2 Ara-3 Ara-4 Ara-5 Ara-6; do sed "s/>REL606/>"$i"/g" ../../all_genomes/"$i"*50000gen*fasta; done >> all_genomes_modded.fasta
-cat ../../all_genomes/REL606.fasta >> all_genomes_modded.fasta
-
-gffread -E -w test_modified.faa -g all_genomes_modded.fasta test_modified.gtf
-
-#Ideally in the form of paired sequences. Each file marked with the mutation name, top one anc, bottom one evo.
-#Feed em thru salis lab.
-
-############blast QC###########
-
-#This needs to be buttressed with blast analysis:
-
-for i in Ara+1 Ara+2 Ara+3 Ara+4 Ara+5 Ara-1 Ara-2 Ara-3 Ara-4 Ara-5 Ara-6; do
-grep -v "#" "$i"_50000gen_[0-9][0-9][0-9][0-9][0-9].gd | sed "s/IS1\t-1/IS1_-1/g" | sed "s/IS1\t1/IS1_1/g" | sed "s/IS3\t-1/IS3_-1/g" | sed "s/IS3\t1/IS3_1/g" | sed "s/IS4\t-1/IS4_-1/g" | sed "s/IS4\t1/IS4_1/g" | sed "s/IS150\t-1/IS150_-1/g" | sed "s/IS150\t1/IS150_1/g" | sed "s/IS186\t-1/IS186_-1/g" | sed "s/IS186\t1/IS186_1/g" | egrep "SNP|INS|MOB" | awk -v var="$i" -F '\t' '{OFS=""}{print "REL606\t.\tCDS\t",$5,"\t",$5+500,"\t.\t+\t0\ttranscript_id \"",var,"_",$2,"_",$1,"_plus_down_500\";gene_id \"",var,"_",$2,"_",$1,"_plus_down_500\";"}' > "$i"_ancestor.gtf
-grep -v "#" "$i"_50000gen_[0-9][0-9][0-9][0-9][0-9].gd | sed "s/IS1\t-1/IS1_-1/g" | sed "s/IS1\t1/IS1_1/g" | sed "s/IS3\t-1/IS3_-1/g" | sed "s/IS3\t1/IS3_1/g" | sed "s/IS4\t-1/IS4_-1/g" | sed "s/IS4\t1/IS4_1/g" | sed "s/IS150\t-1/IS150_-1/g" | sed "s/IS150\t1/IS150_1/g" | sed "s/IS186\t-1/IS186_-1/g" | sed "s/IS186\t1/IS186_1/g" | egrep "SNP|INS|MOB" | awk -v var="$i" -F '\t' '{OFS=""}{print "REL606\t.\tCDS\t",$5-500,"\t",$5,"\t.\t-\t0\ttranscript_id \"",var,"_",$2,"_",$1,"_minus_up_500\";gene_id \"",var,"_",$2,"_",$1,"_minus_up_500\";"}' >> "$i"_ancestor.gtf
-grep -v "#" "$i"_50000gen_[0-9][0-9][0-9][0-9][0-9].gd | sed "s/IS1\t-1/IS1_-1/g" | sed "s/IS1\t1/IS1_1/g" | sed "s/IS3\t-1/IS3_-1/g" | sed "s/IS3\t1/IS3_1/g" | sed "s/IS4\t-1/IS4_-1/g" | sed "s/IS4\t1/IS4_1/g" | sed "s/IS150\t-1/IS150_-1/g" | sed "s/IS150\t1/IS150_1/g" | sed "s/IS186\t-1/IS186_-1/g" | sed "s/IS186\t1/IS186_1/g" | egrep "DEL|CON|SUB|AMP" | awk -v var="$i" -F '\t' '{OFS=""}{print "REL606\t.\tCDS\t",$5+$6,"\t",$5+$6+500,"\t.\t+\t0\ttranscript_id \"",var,"_",$2,"_",$1,"_plus_down_500\";gene_id \"",var,"_",$2,"_",$1,"_plus_down_500\";"}' >> "$i"_ancestor.gtf
-grep -v "#" "$i"_50000gen_[0-9][0-9][0-9][0-9][0-9].gd | sed "s/IS1\t-1/IS1_-1/g" | sed "s/IS1\t1/IS1_1/g" | sed "s/IS3\t-1/IS3_-1/g" | sed "s/IS3\t1/IS3_1/g" | sed "s/IS4\t-1/IS4_-1/g" | sed "s/IS4\t1/IS4_1/g" | sed "s/IS150\t-1/IS150_-1/g" | sed "s/IS150\t1/IS150_1/g" | sed "s/IS186\t-1/IS186_-1/g" | sed "s/IS186\t1/IS186_1/g" | egrep "DEL|CON|SUB|AMP" | awk -v var="$i" -F '\t' '{OFS=""}{print "REL606\t.\tCDS\t",$5-500,"\t",$5,"\t.\t-\t0\ttranscript_id \"",var,"_",$2,"_",$1,"_minus_up_500\";gene_id \"",var,"_",$2,"_",$1,"_minus_up_500\";"}' >> "$i"_ancestor.gtf
-sort -t "_" -nk 3 "$i"_ancestor.gtf -o "$i"_ancestor.gtf
-grep -v "#" "$i"_50000gen-applied.gd | grep -v "INV" | sed "s/IS1\t-1/IS1_-1/g" | sed "s/IS1\t1/IS1_1/g" | sed "s/IS3\t-1/IS3_-1/g" | sed "s/IS3\t1/IS3_1/g" | sed "s/IS4\t-1/IS4_-1/g" | sed "s/IS4\t1/IS4_1/g" | sed "s/IS150\t-1/IS150_-1/g" | sed "s/IS150\t1/IS150_1/g" | sed "s/IS186\t-1/IS186_-1/g" | sed "s/IS186\t1/IS186_1/g" | awk '{ for (i=1; i<=NF; i++) { if (i == 1 || i == 2 || i == 5 || i == 6 || $i ~ /(applied_start=|applied_end=)/) printf "%s ", $i } printf "\n" }' | sed "s/applied_end=//g" | sed "s/applied_start=//g" | awk -v var="$i" -F ' ' '{OFS=""}{print "REL606\t.\tCDS\t",$5,"\t",$5+500,"\t.\t+\t0\ttranscript_id \"",var,"_",$2,"_",$1,"_plus_down_500\";gene_id \"",var,"_",$2,"_",$1,"_plus_down_500\";"}' > "$i"_evolved.gtf
-grep -v "#" "$i"_50000gen-applied.gd | grep -v "INV" | sed "s/IS1\t-1/IS1_-1/g" | sed "s/IS1\t1/IS1_1/g" | sed "s/IS3\t-1/IS3_-1/g" | sed "s/IS3\t1/IS3_1/g" | sed "s/IS4\t-1/IS4_-1/g" | sed "s/IS4\t1/IS4_1/g" | sed "s/IS150\t-1/IS150_-1/g" | sed "s/IS150\t1/IS150_1/g" | sed "s/IS186\t-1/IS186_-1/g" | sed "s/IS186\t1/IS186_1/g" | awk '{ for (i=1; i<=NF; i++) { if (i == 1 || i == 2 || i == 5 || i == 6 || $i ~ /(applied_start=|applied_end=)/) printf "%s ", $i } printf "\n" }' | sed "s/applied_end=//g" | sed "s/applied_start=//g" | awk -v var="$i" -F ' ' '{OFS=""}{print "REL606\t.\tCDS\t",$6-500,"\t",$6,"\t.\t-\t0\ttranscript_id \"",var,"_",$2,"_",$1,"_minus_up_500\";gene_id \"",var,"_",$2,"_",$1,"_minus_up_500\";"}' >> "$i"_evolved.gtf
-sort -t "_" -nk 3 "$i"_evolved.gtf -o "$i"_evolved.gtf
-done
-
-grep -v "#" "$i"_50000gen-applied.gd | grep -v "INV" | sed "s/IS1\t-1/IS1_-1/g" | sed "s/IS1\t1/IS1_1/g" | sed "s/IS3\t-1/IS3_-1/g" | sed "s/IS3\t1/IS3_1/g" | sed "s/IS4\t-1/IS4_-1/g" | sed "s/IS4\t1/IS4_1/g" | sed "s/IS150\t-1/IS150_-1/g" | sed "s/IS150\t1/IS150_1/g" | sed "s/IS186\t-1/IS186_-1/g" | sed "s/IS186\t1/IS186_1/g" | awk '{ for (i=1; i<=NF; i++) { if (i == 1 || i == 2 || i == 5 || i == 6 || $i ~ /(applied_start=|applied_end=)/) printf "%s ", $i } printf "\n" }' | sed "s/applied_end=//g" | sed "s/applied_start=//g" | awk -v var="$i" -F ' ' '{OFS=""}{print "REL606\t.\tCDS\t",$5,"\t",$5+500,"\t.\t+\t0\ttranscript_id \"",var,"_",$2,"_",$1,"_plus_down_500\";gene_id \"",var,"_",$2,"_",$1,"_plus_down_500\";"}' > "$i"_evolved.gtf
-grep -v "#" "$i"_50000gen-applied.gd | grep -v "INV" | sed "s/IS1\t-1/IS1_-1/g" | sed "s/IS1\t1/IS1_1/g" | sed "s/IS3\t-1/IS3_-1/g" | sed "s/IS3\t1/IS3_1/g" | sed "s/IS4\t-1/IS4_-1/g" | sed "s/IS4\t1/IS4_1/g" | sed "s/IS150\t-1/IS150_-1/g" | sed "s/IS150\t1/IS150_1/g" | sed "s/IS186\t-1/IS186_-1/g" | sed "s/IS186\t1/IS186_1/g" | awk '{ for (i=1; i<=NF; i++) { if (i == 1 || i == 2 || i == 5 || i == 6 || $i ~ /(applied_start=|applied_end=)/) printf "%s ", $i } printf "\n" }' | sed "s/applied_end=//g" | sed "s/applied_start=//g" | awk -v var="$i" -F ' ' '{OFS=""}{print "REL606\t.\tCDS\t",$6-500,"\t",$6,"\t.\t-\t0\ttranscript_id \"",var,"_",$2,"_",$1,"_minus_up_500\";gene_id \"",var,"_",$2,"_",$1,"_minus_up_500\";"}' >> "$i"_evolved.gtf
+cat /stor/work/Ochman/hassan/LTEE_analysis/LTEE_data/all_genomes/REL606.fasta >> all_genomes_modded.faa
+cat 100bp/*evolved*htseqread*gtf 200bp/*evolved*htseqread*gtf | grep -f upregs | gtf2bed | awk '{OFS=""}{print $4,"_",$0}' | cut -f 1,7- -d "_" | sed "s/_REL606\t/\t/g" | bedtools getfasta -fi all_genomes_modded.faa -bed - -s -name > upregs.faa
 
 for i in Ara+1 Ara+2 Ara+3 Ara+4 Ara+5 Ara-1 Ara-2 Ara-3 Ara-4 Ara-5 Ara-6
 do
-gffread -E -w "$i"_ancestor.faa -g ../../../all_genomes/REL606.fasta "$i"_ancestor.gtf
-/stor/work/Ochman/hassan/tools/gmap-2021-05-27/bin/gmap -D .. -d "$i" -f 2 --gff3-fasta-annotation=1 "$i"_ancestor.faa > "$i"_ancestor.gff3 #map to evolved genomes
-awk -F '\t' '($3=="mRNA")' "$i"_ancestor.gff3 | grep "mrna1" | cut -f 1-3 -d '.' | awk -F '\t' '{OFS=FS}{print $1,$2=".",$3="CDS",$4,$5,$6,$7,$8,"transcript_id \"",$9,"\";gene_id \"",$9,"\";"}' | sed "s/\tID=//g" | sed "s/\t\"/\"/g" > "$i"_evolved_gmapped.gtf
+grep --no-group-separator -A1 "$i" upregs.faa > "$i"_upregs.faa
+/stor/work/Ochman/hassan/tools/gmap-2021-05-27/bin/gmap -D . -d "$i" -f 2 --gff3-fasta-annotation=1 "$i"_upregs.faa > "$i"_upregs.gff3 #map to evolved genomes
 done
+
+cat *upregs.gff3 | grep "mRNA.*path2" | cut -f 1 -d '(' | cut -f 2 -d "=" | sort -u > duplicates
+
+#Manually check whether closely neighboring mutations (within <20 bp of each other) have been double-counted, and remove them:
+
+cat 100bp/*evolved*htseqread*gtf 200bp/*evolved*htseqread*gtf | grep -f upregs | gtf2bed | awk '{OFS=""}{print $4,"_",$0}' | cut -f 1,7- -d "_" | sed "s/_REL606\t/\t/g" | sort -nk 4
+
+Ara+3_986_DEL
+Ara+3_987_INS
+Ara+3_205_SNP
+Ara-2_164_INS
+Ara-4_770_DEL
+Ara-4_771_INS
+
+#Manually add them to the "duplicates" file.
+
+#Finally, remove cases of overlap with repeat regions on the opposite strand:
+
+for i in Ara+1 Ara+2 Ara+3 Ara+4 Ara+5 Ara-1 Ara-2 Ara-3 Ara-4 Ara-5 Ara-6; do sed "s/^/"$i"\t/g" /stor/work/Ochman/hassan/LTEE_analysis/LTEE_data/converted_gtf_files/"$i"*50000*gtf | grep "repeat_region" | cut -f 1,3- >> all_repeat_regions.gtf; done
+cat 100bp/*evolved*htseqread*gtf 200bp/*evolved*htseqread*gtf | grep -f upregs | gtf2bed | awk '{OFS=""}{print $4,"_",$0}' | cut -f 1,7- -d "_" | sed "s/_REL606\t/\t/g" | sort -nk 4 | bedtools intersect -wo -a - -b all_repeat_regions.gtf | awk -F '\t' '($20>90)' | cut -f 2 -d "\"" | rev | cut -f 2- -d "_" | rev | sort -u >> duplicates
+
+grep -v -f duplicates upregs | rev | cut -f 2- -d "_" | sort -u | rev > 63_coverage_candidates.txt
+
+#Make coverage plots from this point on.
