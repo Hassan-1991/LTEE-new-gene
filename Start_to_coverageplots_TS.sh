74e52816c9d207a4910bcb54ac5ff34298e6b3cd
Hassan-1991 <67057211+Hassan-1991@users.noreply.github.com>
2023-07-30 13:48:21 -0500

    Update Start_to_coverageplots_TS.sh

--- Start_to_coverageplots_TS.sh
+++ Start_to_coverageplots_TS.sh
@@ -47,3 +47,30 @@ ls ../../../../../trimmomatic_29bp_trimmed/bamfiles/namesorted/*sorted.bam | egr
 ls ../../../../../50k_bowtie_indices_bamfiles/*sorted.bam | grep "Ara-3" | sed "s/^/time htseq-count -f bam -a 0 -t CDS --nonunique all /g" | awk '{OFS=""}{print $0," Ara-3_50000gen_11364_htseqready.gtf \| head -n -5 \> T",$11}' | sed "s/T..\/..\/..\/..\/..\/50k_bowtie_indices_bamfiles\///g" | sed "s/$/t/g" | sed "s/_sorted.bamt//g" | awk '{OFS=""}{print $0,"_htseq_raw.tsv"}' > 50k_ts_htseq.sh
 ls ../../../../../50k_bowtie_indices_bamfiles/*sorted.bam | grep "REL60" | sed "s/^/time htseq-count -f bam -a 0 -t CDS --nonunique all /g" | awk '{OFS=""}{print $0," REL606_htseqready.gtf \| head -n -5 \> T",$11}' | sed "s/T..\/..\/..\/..\/..\/50k_bowtie_indices_bamfiles\///g" | sed "s/$/t/g" | sed "s/_sorted.bamt//g" | awk '{OFS=""}{print $0,"_htseq_raw.tsv"}' >> 50k_ts_htseq.sh
 
+#Now add in deleted genes so the number of genes are equal across all, ascribe 0 counts to them
+
+for i in $(ls Ara-3*htseqready.gtf | cut -f 1-3 -d "_")
+do
+cut -f 2 -d "\"" "$i"_htseqready.gtf | grep -v "ECB" > temp && grep -v -f temp nondup_time_series_mutation_ids_ancestor.gtf | cut -f 2 -d "\"" | sed "s/$/\t0/g" >> "$i"_BiolRep1*raw.tsv
+cut -f 2 -d "\"" "$i"_htseqready.gtf | grep -v "ECB" > temp && grep -v -f temp nondup_time_series_mutation_ids_ancestor.gtf | cut -f 2 -d "\"" | sed "s/$/\t0/g" >> "$i"_BiolRep2*raw.tsv
+cut -f 2 -d "\"" "$i"_htseqready.gtf | grep -v "ECB" > temp && grep -v -f temp nondup_time_series_mutation_ids_ancestor.gtf | cut -f 2 -d "\"" | sed "s/$/\t0/g" >> "$i"_BiolRep3*raw.tsv
+done
+
+cut -f 2 -d "\"" Ara-3_50000*_htseqready.gtf | grep -v "ECB" > temp && grep -v -f temp nondup_time_series_mutation_ids_ancestor.gtf | cut -f 2 -d "\"" | sed "s/$/\t0/g" >> rep1-ribo-Ara-3_htseq_raw.tsv
+cut -f 2 -d "\"" Ara-3_50000*_htseqready.gtf | grep -v "ECB" > temp && grep -v -f temp nondup_time_series_mutation_ids_ancestor.gtf | cut -f 2 -d "\"" | sed "s/$/\t0/g" >> rep2-ribo-Ara-3_htseq_raw.tsv
+cut -f 2 -d "\"" Ara-3_50000*_htseqready.gtf | grep -v "ECB" > temp && grep -v -f temp nondup_time_series_mutation_ids_ancestor.gtf | cut -f 2 -d "\"" | sed "s/$/\t0/g" >> rep1-rna-Ara-3_htseq_raw.tsv
+cut -f 2 -d "\"" Ara-3_50000*_htseqready.gtf | grep -v "ECB" > temp && grep -v -f temp nondup_time_series_mutation_ids_ancestor.gtf | cut -f 2 -d "\"" | sed "s/$/\t0/g" >> rep2-rna-Ara-3_htseq_raw.tsv
+
+#Add in line, replicate and generation information
+
+ls Ara-3*BiolRep*tsv | cut -f 1-4 -d "_" | awk -F '_' '{OFS=""}{print "sed -i \"s\/^\/",$1,"\\t",$2,"\\t",$3,"\\t",$4,"\\t\/g\""}' | paste -d '\t' - <(ls Ara-3*BiolRep*tsv) | awk '{print $1,$2,$3,$4}' | bash
+ls REL606*BiolRep*tsv | cut -f 1-4 -d "_" | awk -F '_' '{OFS=""}{print "sed -i \"s\/^\/\REL606\\t0gen\\t",$1,"\\t",$2,"\\t\/g\""}' | paste -d '\t' - <(ls REL606*BiolRep*tsv) | awk '{print $1,$2,$3,$4}' | bash
+
+#Run DeSeq2 on the TS dataset!
+
+###
+
+#Get all increases (candidate proto-genes)
+
+cat 100bp_timeseries/TIMESERIES_deseq2_results.csv 200bp_timeseries/TIMESERIES_deseq2_results.csv | awk -F ',' '($8<0.05&&$5>0&&$3!~"ECB")' | cut -f 3 -d ',' | sort -u > 12_increases.txt
+
