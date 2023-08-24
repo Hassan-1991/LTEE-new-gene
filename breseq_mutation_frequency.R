0968d32d03296b13edac74a0babc8af74c347412
Hassan-1991 <67057211+Hassan-1991@users.noreply.github.com>
2023-08-24 15:08:24 -0500

    Update breseq_mutation_frequency.R

--- breseq_mutation_frequency.R
+++ breseq_mutation_frequency.R
@@ -12,25 +12,29 @@ grep "1881296" Ara-2*K/output/*gd | grep "MOB.*frequency=" | sed "s/K\//_/g" | s
 
 #R code (WIP)
 
+library(dplyr)
 library(tidyverse)
+library(ggplot2)
+library(gridExtra)  # Load the gridExtra package for grid.arrange()
 
-getwd()
 setwd("/stor/scratch/Ochman/hassan/Good2017_LTEE_data/trimmed")
 
-p1 <- read.csv("breseq_10protogenes.tsv", sep = '\t')
+p1 <- read.csv("breseq_10protogenes_curated.tsv", sep = '\t')
 
 values_to_plot <- c("Ara-2_118_SNP","Ara-2_610_SNP","Ara-2_731_SNP","Ara-3_547_MOB","Ara-6_24_MOB","Ara-6_59_DEL","Ara+1_94_DEL","Ara+1_99_MOB","Ara+5_30_MOB","Ara+5_65_MOB")
+
+plots_list <- list()  # Create an empty list to store the plots
 
 for (value in values_to_plot) {
   plot <- p1 %>%
     filter(target_id == value) %>%
     ggplot(aes(x = Generation, y = Allele_freq / 100)) +
-    geom_line() +
-    scale_x_continuous(breaks = seq(0, 60000, 500)) +
-    labs(title = value) +
+    geom_line(size = 1.5) +
+    scale_x_continuous(breaks = seq(0, 60000, 5000)) +
+    labs(title = value, x = "Generation", y = "Allele frequency") +
+    ylim(0, 1) +
     theme(
-      panel.background = element_rect(fill = 'transparent'),
-      plot.background = element_rect(fill = 'transparent', color = NA),
+      plot.background = element_blank(),  # Transparent plot background
       panel.grid.minor = element_blank(),
       plot.title = element_text(hjust = 0.5),
       legend.background = element_rect(fill = 'transparent'),
@@ -41,8 +45,16 @@ for (value in values_to_plot) {
       axis.text.y = element_text(size = 16),
       legend.key.size = unit(1, 'cm'),
       legend.text = element_text(size = 15),
-      legend.position = "top"
+      legend.position = "top",
+      plot.margin = margin(1, 1, 1, 1),  # Adjust the margins around the plot
+      panel.background = element_rect(color = "black", size = 1)  # Add a black border around the plot area
     )
   
-  ggsave(paste0(value, ".png"), plot, width = 30, height = 6, unit = "in")
+  plots_list[[value]] <- plot  # Add the plot to the list
 }
+
+# Create a collage by arranging all the plots in a grid
+collage <- do.call(grid.arrange, c(plots_list, ncol = 1))
+
+# Save the collage
+ggsave("collage.pdf", collage, device="pdf", width = 20, height = 60, units = "in", limitsize=FALSE)
