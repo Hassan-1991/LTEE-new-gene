eee685e33ae2b1c52eb8d709097711581ca6eaff
Hassan-1991 <67057211+Hassan-1991@users.noreply.github.com>
2024-02-13 11:32:00 -0600

    Update 1_Window_analysis.sh

--- 1_Window_analysis.sh
+++ 1_Window_analysis.sh
@@ -4,28 +4,28 @@
 
 awk '!/^>/ { printf "%s", $0; n="\n"} /^>/ {print n $0; n = "" } END {printf "%s", n }' REL606.fasta | grep -v "^>" | wc
 
-#The value this returns is 4629813. We use this and the window size, 400, to write the next piece of code
-#Construct a gtf file with all 400-bp windows across both strands of the genome
-#Get the corresponding sequences
-
 seq 1 400 4629813 > col1 #Start coordinate of each window
 seq 1 400 4629813 | tail -n+2 > col2 #Stop coordinate of each window
 paste col1 col2 | cat -n | sed "s/ //g" | awk -F '\t' '{OFS=FS}{print "REL606\t.\tCDS",$2,$3-1,".\t+\t0\tgene_id \"window_plus_",$1,"\";transcript_id \"window_plus_",$1,"\";"}' | head -n-1 | sed "s/_\t/_/g" | sed "s/\t\"/\"/g" > REL606_slidwindow400.gtf #Windows on plus strand
 paste col1 col2 | cat -n | sed "s/ //g" | awk -F '\t' '{OFS=FS}{print "REL606\t.\tCDS",$2,$3-1,".\t-\t0\tgene_id \"window_minus_",$1,"\";transcript_id \"window_minus_",$1,"\";"}' | head -n-1 | sed "s/_\t/_/g" | sed "s/\t\"/\"/g" >> REL606_slidwindow400.gtf #Windows on minus strand
 rm col* #remove intermediate files
+bedtools intersect -a REL606_slidwindow400.gtf -b REL606.gtf -wo | egrep "repeat_region|IS" | cut -f9 | sort -u | grep -vf - REL606_slidwindow400.gtf > test && mv test REL606_slidwindow400.gtf
 gffread -E -w REL606_slidwindow400_CDS.faa -g REL606.fasta REL606_slidwindow400.gtf #get their sequences
 
 #Step-2: From this file, get rid of all windows that map more than once in any genome, experienced insertions or deletions, diverged substantially, or got deleted from any evolved genome
 
 #We first map the window sequences to each evolved genome with gmap
 
-for i in Ara+1 Ara+2 Ara+3 Ara+4 Ara+5 Ara-1 Ara-2 Ara-3 Ara-4 Ara-5 Ara-6;
-do gmap_build -D . -d "$i" "$i".faa #build gmap indices for each evolved genome
-gmap -D . -d "$i" -f 2 --gff3-fasta-annotation=1 REL606_slidwindow400_CDS.faa > "$i"_slidwindow400_CDS.gff3 #map to evolved genomes
-grep "mRNA" "$i"_slidwindow400_CDS.gff3 | grep "path2" | cut -f 2 -d '=' | cut -f 1 -d '.' | sed "s/^/=/g" | sed "s/$/;/g" > "$i"_400_doublemaps #identify all windows that map more than once in any evolved genome
-grep -v -f "$i"_400_doublemaps "$i"_slidwindow400_CDS.gff3 | grep "mRNA" > "$i"_400_step1 #subtract "doublemaps"
-awk -F '\t' '(($5-$4>410)||($5-$4<390))' "$i"_400_step1 > "$i"_400_longshortmaps #identify the windows that map with a length +/-10bp
-grep -v -f "$i"_400_longshortmaps "$i"_400_step1 > "$i"_400_step2; done #Subtract "longshortmaps"
+variables=("Ara+1" "Ara+2" "Ara+3" "Ara+4" "Ara+5" "Ara-1" "Ara-2" "Ara-3" "Ara-4" "Ara-5" "Ara-6")
+for var in "${variables[@]}"; do
+echo "/stor/work/Ochman/hassan/tools/gmap-2021-05-27/bin/gmap -D . -d $var -f 2 --gff3-fasta-annotation=1 REL606_slidwindow400_CDS.faa > ${var}_slidwindow400_CDS.gff3"
+echo "grep \"mRNA\" ${var}_slidwindow400_CDS.gff3 | grep \"path2\" | cut -f 2 -d '=' | cut -f 1 -d '.' | sed 's/^/=/g' | sed 's/\$/;/g' > ${var}_400_doublemaps"
+echo "grep -v -f ${var}_400_doublemaps ${var}_slidwindow400_CDS.gff3 | grep \"mRNA\" > ${var}_400_step1"
+echo "awk -F '\t' '(\$5-\$4>410)||(\$5-\$4<390)' ${var}_400_step1 > ${var}_400_longshortmaps"
+echo "grep -v -f ${var}_400_longshortmaps ${var}_400_step1 > ${var}_400_step2"; done | split -l 5 -
+
+#Run the resulting x-files in separate screens
+
 rm *longshortmaps
 rm *doublemaps #Remove interim files
 
@@ -44,10 +44,14 @@ cat Ara*400*step2 | cut -f 2,9 | cut -f 1 -d ';' | sed "s/.mrna1//g" | sed "s/ID
 #Extract these windows from the gff3s constructed earlier, convert them into gtfs
 
 for i in Ara+1 Ara+2 Ara+3 Ara+4 Ara+5 Ara-1 Ara-2 Ara-3 Ara-4 Ara-5 Ara-6;
-do grep -f common_400bp_windows.txt "$i"_400_step2grep "mRNA" "$i"_400_commonwindows.gff3 | sed "s/ID=/transcript_id=\"/g" | sed "s/.mrna1;Name=/\";gene_id \"/g" | sed "s/;Parent/\";Parent/g" | sed "s/\tmRNA\t/\tCDS\t/g" | awk -F '\t' '{OFS=FS}{print $1,$2=".",$3,$4,$5,$6,$7,$8,$9}' > "$i"_400_commonwindows.gtf; done
+do grep -f common_400bp_windows.txt "$i"_400_step2 | sed "s/ID=/transcript_id=\"/g" | sed "s/.mrna1;Name=/\";gene_id \"/g" | sed "s/;Parent/\";Parent/g" | sed "s/\tmRNA\t/\tCDS\t/g" | awk -F '\t' '{OFS=FS}{print $1,$2=".",$3,$4,$5,$6,$7,$8,$9}' > "$i"_400_commonwindows.gtf; done
 
-#Generate the feature lists for two ancestors manually
+#Generate the feature lists for two ancestors
 
 sed "s/=/\"/g" common_400bp_windows.txt | sed "s/;/\"/g" > common_400bp_windows_2.txt 
 grep -f common_400bp_windows_2.txt REL606_slidwindow400.gtf > REL606_400_commonwindows.gtf 
 cp REL606_400_commonwindows.gtf REL607_400_commonwindows.gtf
+
+#BOWTIE AND HTSEQ STEPS#
+
+
